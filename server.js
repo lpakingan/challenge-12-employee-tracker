7b902b04f105eed91fb3baae98006efbe1dd0cd0
lpakingan <lpak98@gmail.com>
2023-06-23 14:12:53 -0700

    debug issue with inquirer prompt closing immediately, include better error message feedback

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const CLI = require('./lib/cli');
 // create the port to run the application
 const PORT = process.env.PORT || 3001;
 
-// express the middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
 // connect to the employee database
 const db = mysql.createConnection(
   {
@@ -20,9 +16,17 @@ const db = mysql.createConnection(
     password: '123',
     // the employee database that is being called
     database: 'employee_db'
-  },
-  console.log(`Connected to the employee_db database.`)
-);
+  });
 
-// run the command line interface for user input
- new CLI().run();
+  // check if a successful connection was established to the database
+  db.connect((error) => {
+    // if error in connection, gives the user back the error
+    if (error) {
+      console.error('Error connecting to the database: ', error);
+      return;
+    } else {
+    // if connection was successful, run the command line interface for user input
+    console.log(`Now connected to the employee database through PORT ${PORT}!`);
+    new CLI().run();
+    }
+  });
