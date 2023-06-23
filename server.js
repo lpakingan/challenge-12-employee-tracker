0f5ab8bf2eaf2c5be4326a5f4621f7eb67fa4385
lpakingan <lpak98@gmail.com>
2023-06-23 11:08:21 -0700

    add initial code for server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// import the required packages
+const express = require('express');
+const mysql = require ('mysql12');
+
+// create the port to run the application and run express
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express the middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// connect to the employee database
+const db = mysql.createConnection(
+  {
+    // MySQL host
+    host: 'localhost',
+    // MySQL username 
+    user: 'root',
+    // MySQL password
+    password: '123',
+    // the employee database that is being called
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
