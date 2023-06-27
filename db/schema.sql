f9e1d3b8eaa75acec2c0e5b5a603dbb882048f70
lpakingan <lpak98@gmail.com>
2023-06-27 12:00:31 -0700

    add bonus delete functions

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,8 @@ CREATE TABLE department (
     department_name VARCHAR(30) NOT NULL 
 );
 
+-- create role table
+-- on delete is set to cascading to account for when the department the role is in is deleted
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
@@ -15,9 +17,11 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL
+    ON DELETE CASCADE
 );
 
+-- create employee table
+-- on delete is set to cascading to account for when the role is deleted
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -25,7 +29,8 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role (id),
+    REFERENCES role (id)
+    ON DELETE CASCADE,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
